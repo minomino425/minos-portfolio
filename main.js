7b6a5576411a82795e743d9f3b7d2ba111ce591d
蓑田朋未 <minodatomomi@minodatomominoMacBook-Air.local>
2022-06-28 08:44:56 +0900

    all done

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //canvas
 const canvas = document.querySelector("#webgl");
 import * as THREE from "three";
-import imgUrl from './img/pic.jpg'
+import imgUrl from "./img/pic.jpg";
+import hamUrl from "./img/ham.png";
 
 init();
 async function init() {
@@ -19,26 +20,83 @@ async function init() {
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const geometry1 = new THREE.PlaneGeometry(3, 5);
-  const geometry2 = new THREE.PlaneGeometry(3, 5);
-  const geometry3 = new THREE.PlaneGeometry(3, 5);
-  const geometry4 = new THREE.PlaneGeometry(3, 5);
-  const geometry5 = new THREE.PlaneGeometry(3, 5);
-  const geometry6 = new THREE.PlaneGeometry(3, 5);
+  let geometry;
+  for (let i = 0; i < 6; ++i) {
+    geometry = new THREE.PlaneGeometry(3, 4.2);
+  }
 
   const material = new THREE.MeshBasicMaterial({
     map: new THREE.TextureLoader().load(imgUrl),
   });
-  const cube1 = new THREE.Mesh(geometry1, material);
-  const cube2 = new THREE.Mesh(geometry2, material);
-  const cube3 = new THREE.Mesh(geometry3, material);
-  const cube4 = new THREE.Mesh(geometry4, material);
-  const cube5 = new THREE.Mesh(geometry5, material);
-  const cube6 = new THREE.Mesh(geometry6, material);
+  const hamMaterial = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(hamUrl),
+  });
+
+  const cube1 = new THREE.Mesh(geometry, material);
+  const cube2 = new THREE.Mesh(geometry, material);
+  const cube3 = new THREE.Mesh(geometry, material);
+  const cube4 = new THREE.Mesh(geometry, material);
+  const cube5 = new THREE.Mesh(geometry, material);
+  const cube6 = new THREE.Mesh(geometry, material);
 
   scene.add(cube1, cube2, cube3, cube4, cube5, cube6);
 
+  const planeArray = [cube1, cube2, cube3, cube4, cube5, cube6];
+
   camera.position.z = 30;
+
+  // Raycaster のインスタンスを生成する @@@
+  const raycaster = new THREE.Raycaster();
+  // マウスのクリックイベントの定義 @@@
+  // window.addEventListener(
+  //   "click",
+  //   (mouseEvent) => {
+  //     // スクリーン空間の座標系をレイキャスター用に正規化する（-1.0 ~ 1.0 の範囲）
+  //     const x = (mouseEvent.clientX / window.innerWidth) * 2.0 - 1.0;
+  //     const y = (mouseEvent.clientY / window.innerHeight) * 2.0 - 1.0;
+  //     // 上下が反転している点に注意
+  //     const v = new THREE.Vector2(x, -y);
+  //     // レイキャスターに正規化済みマウス座標とカメラを指定する
+  //     raycaster.setFromCamera(v, camera);
+  //     // scene に含まれるすべてのオブジェクトを対象にレイキャストする
+  //     const intersects = raycaster.intersectObjects(planeArray);
+  //     // レイが交差しなかった場合を考慮し一度マテリアルをリセットしておく
+  //     planeArray.forEach((mesh) => {
+  //       mesh.material = material;
+  //     });
+  //     if (intersects.length > 0) {
+  //       intersects[0].object.material = hamMaterial;
+  //       console.log(intersects[0].object);
+  //     }
+  //   },
+  //   false
+  // );
+  window.addEventListener(
+    "mousemove",
+    (mouseEvent) => {
+      // スクリーン空間の座標系をレイキャスター用に正規化する（-1.0 ~ 1.0 の範囲）
+      const x = (mouseEvent.clientX / window.innerWidth) * 2.0 - 1.0;
+      const y = (mouseEvent.clientY / window.innerHeight) * 2.0 - 1.0;
+      // 上下が反転している点に注意
+      const v = new THREE.Vector2(x, -y);
+      // レイキャスターに正規化済みマウス座標とカメラを指定する
+      raycaster.setFromCamera(v, camera);
+      // scene に含まれるすべてのオブジェクトを対象にレイキャストする
+      const intersects = raycaster.intersectObjects(planeArray);
+      // レイが交差しなかった場合を考慮し一度マテリアルをリセットしておく
+      planeArray.forEach((mesh) => {
+        mesh.material = material;
+        document.querySelector("body").style.cursor = "initial";
+      });
+      if (intersects.length > 0) {
+        intersects[0].object.material = hamMaterial;
+        console.log(intersects[0].object);
+        document.querySelector("body").style.cursor = "pointer";
+      }
+    },
+    false
+  );
+
   //サイズ
   const sizes = {
     width: window.innerWidth,
@@ -57,20 +115,16 @@ async function init() {
     rotation += speed;
     speed *= 0.93;
     //ジオメトリ全体を回転させる
-    cube1.position.x = 8 * Math.cos(rotation + Math.PI / 3);
-    cube1.position.z = 8 * Math.sin(rotation + Math.PI / 3);
-    cube2.position.x = 8 * Math.cos(rotation + 2 * (Math.PI / 3));
-    cube2.position.z = 8 * Math.sin(rotation + 2 * (Math.PI / 3));
-    cube3.position.x = 8 * Math.cos(rotation + Math.PI);
-    cube3.position.z = 8 * Math.sin(rotation + Math.PI);
-    cube4.position.x = 8 * Math.cos(rotation + 4 * (Math.PI / 3));
-    cube4.position.z = 8 * Math.sin(rotation + 4 * (Math.PI / 3));
-    cube5.position.x = 8 * Math.cos(rotation + 5 * (Math.PI / 3));
-    cube5.position.z = 8 * Math.sin(rotation + 5 * (Math.PI / 3));
-    cube6.position.x = 8 * Math.cos(rotation);
-    cube6.position.z = 8 * Math.sin(rotation);
-
-    //   mesh1.position.x = rotation;
+    const mathPositionRatio = (cube, multi) => {
+      cube.position.x =
+        8 * Math.cos(rotation + multi * (Math.PI / 3) + Math.PI / 6);
+      cube.position.z =
+        8 * Math.sin(rotation + multi * (Math.PI / 3) + Math.PI / 6);
+    };
+    let num = 1;
+    planeArray.forEach((cube, index) => {
+      mathPositionRatio(cube, index + num);
+    });
     window.requestAnimationFrame(rot);
   }
 
